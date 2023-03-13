67adf49ad9097fd5487132662ccabb9f043107ad
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-03-13 16:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """
 Bunch of functions
 """
+# just commenting
 
 def add(number_a:int, number_b:int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b
+    return number_a  + number_b + 1
