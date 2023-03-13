be839f292ed62a4f9bb2778f3c974ab7cbeadc6d
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-03-13 10:22:29 +0000

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Bunch of functions
+"""
+
+def add(number_a:int, number_b:int):
+    """
+    This function adds 2 numbers
+    """
+    return number_a  + number_b
+    
---
+++ testing.py
@@ -0,0 +1,7 @@
+"""testing functions"""
+from main import add
+
+def test_add():
+    """testing add function"""
+    assert add(2,3) == 5
+    
