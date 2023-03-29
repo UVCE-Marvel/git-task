6d41cff5eb4e758d9fccca72fda3f1701a48f421
prpawan03 <99591052+prpawan03@users.noreply.github.com>
2023-03-29 12:00:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 """
 Bunch of functions
 """
+number_a = int(input("enter number1"))
+number_b = int(input("enter number2"))
+
 
 def add(number_a:int, number_b:int):
     """
     This function adds 2 numbers
     """
     return number_a  + number_b 
+    
+result = add(number_a, number_b)
+print(result)
     
