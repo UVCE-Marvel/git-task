51c14122c9a695c6f1aa61501862c205265c11d7
sammyGuru <129886886+sammyGuru@users.noreply.github.com>
2023-04-04 23:22:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Bunch of functions
 """
 
-def add(number_a:int, number_b:int):
+def add(number1:int, number2:int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b 
+    return number1  + number2
     
