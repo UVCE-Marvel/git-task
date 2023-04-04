32845e9210e5e9cfb09bff6656cfc5f4f230fe2b
Aryan Aithal <aryanaithal.12@gmail.com>
2023-04-04 22:18:57 +0530

    changed the name of the variable as there were no error

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Bunch of functions
 """
 
-def add(number_a:int, number_b:int):
+def add(num1:int, num2:int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b 
+    return ( num1  + num2 )
     
