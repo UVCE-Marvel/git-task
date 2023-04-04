9f15d9d75004687d538facdb156f6f4d985b0d0c
Vinay Basavaraddi <vinay.basavaraddi@gmail.com>
2023-04-04 16:11:03 +0530

    There is no error in main branch

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 Bunch of functions
 """
 
-def add(number_a:int, number_b:int):
+def add(number_a:int, number_b:int) -> int:
     """
-    This function adds 2 numbers
-    """
-    return number_a  + number_b 
     
+    This function adds 2 numbers in their int format and returns their sum
+
+    """
+    return number_a + number_b 
