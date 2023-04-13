ef703dc4d112cb582de426d7d7ec5bd507d0f2bf
fathimaalzahra <115719907+fathimaalzahra@users.noreply.github.com>
2023-04-13 16:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Bunch of functions
 
 def add(number_a:int, number_b:int):
     """
-    This function adds 2 numbers
+    This function subtracts 2 numbers
     """
-    return number_a  + number_b 
+    return number_a  - number_b 
     
