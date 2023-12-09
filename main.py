382a9f81030626c4f43232a549e64ef88ad3200a
mythili953 <mythilishetty953@gmail.com>
2023-12-09 10:06:20 +0530

    main error

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Bunch of functions
 """
 
-def add(number_a:int, number_b:int):
+def add(number_a: int, number_b: int):
     """
     This function adds 2 numbers
     """
-    return number_a  + number_b + 1
+    return number_a  + number_b 
     
